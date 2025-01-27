df8fccb45ae8a873e006fcc4ce5125dc9f8a1485
christiaylor <tay10r@byu.edu>
2025-01-27 09:02:10 -0700

    lots

--- support.cs
+++ support.cs
@@ -2,20 +2,53 @@ namespace Mission4Ass;
 
 public class Support
 {
-    public Support()
+    public char[,] BoardArray = new char[3, 3];
+
+    public Support(char[,] boardArray)
         {
-        char[,] boardArray = new char[3, 3];
+        BoardArray = boardArray;
         }
-    public void printBoard(char[,] boardArray)
+    public void printBoard()
     {
+
         Console.WriteLine("     |     |     ");
-        Console.WriteLine($"  {boardArray[0, 0]}  |  {boardArray[0, 1]}  |  {boardArray[0, 2]}  ");
+        Console.WriteLine($"  {BoardArray[0, 0]}  |  {BoardArray[0, 1]}  |  {BoardArray[0, 2]}  ");
         Console.WriteLine("_____|_____|_____");
-        Console.WriteLine("     |     |     ");
-        Console.WriteLine($"  {boardArray[1, 0]}  |  {boardArray[1, 1]}  |  {boardArray[1, 2]}  ");
+        Console.WriteLine($"  {BoardArray[1, 0]}  |  {BoardArray[1, 1]}  |  {BoardArray[1, 2]}  ");
         Console.WriteLine("_____|_____|_____");
+        Console.WriteLine($"  {BoardArray[2, 0]}  |  {BoardArray[2, 1]}  |  {BoardArray[2, 2]}  ");
         Console.WriteLine("     |     |     ");
-        Console.WriteLine($"  {boardArray[2, 0]}  |  {boardArray[2, 1]}  |  {boardArray[2, 2]}  ");
-        Console.WriteLine("     |     |     ");
+    }
+
+    public bool checkWinner()
+    {
+        bool winner = false;
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (BoardArray[0, i] == BoardArray[1, i] && BoardArray[0, i] == BoardArray[2, i])
+            {
+                winner = true;
+            }
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            if (BoardArray[i, 0] == BoardArray[i, 1] && BoardArray[i, 0] == BoardArray[i, 2])
+            {
+                winner = true;
+            }
+        }
+
+        if (BoardArray[0, 0] == BoardArray[1, 1] && BoardArray[0, 0] == BoardArray[2, 2])
+        {
+            winner = true;
+        }
+
+        if (BoardArray[2, 0] == BoardArray[1, 1] && BoardArray[2, 0] == BoardArray[0, 2])
+        {
+            winner = true;
+        }
+
+        return winner;
     }
 }
