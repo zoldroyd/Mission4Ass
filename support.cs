73417229153748480eccc7ba8f3066b67c500f4f
christiaylor <tay10r@byu.edu>
2025-01-27 08:34:12 -0700

    Initial Commit

--- support.cs
+++ support.cs
@@ -1,6 +1,21 @@
 namespace Mission4Ass;
 
-public class support
+public class Support
 {
-    
+    public Support()
+        {
+            
+        }
+    public void printBoard(Char[,] boardArray)
+    {
+        Console.WriteLine("     |     |     ");
+        Console.WriteLine($"  {boardArray[0, 0]}  |  {boardArray[0, 1]}  |  {boardArray[0, 2]}  ");
+        Console.WriteLine("_____|_____|_____");
+        Console.WriteLine("     |     |     ");
+        Console.WriteLine($"  {boardArray[1, 0]}  |  {boardArray[1, 1]}  |  {boardArray[1, 2]}  ");
+        Console.WriteLine("_____|_____|_____");
+        Console.WriteLine("     |     |     ");
+        Console.WriteLine($"  {boardArray[2, 0]}  |  {boardArray[2, 1]}  |  {boardArray[2, 2]}  ");
+        Console.WriteLine("     |     |     ");
+    }
 }
