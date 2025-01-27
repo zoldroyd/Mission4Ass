a3b9fb6b868f2521ecd59906ab5aa810149929d1
Zac Oldroyd <137462136+zoldroyd@users.noreply.github.com>
2025-01-27 08:38:37 -0700

    initial instance call

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Mission4Ass;
+ 
+Console.WriteLine("Welcome to the tic-tac-toe game!");
+
+Support sp = new Support();
+
+//Console.WriteLine("Player 1 will use X's and player 2 will use O's");
+//Console.WriteLine("Player 1 please place for first X");
+
+
+
